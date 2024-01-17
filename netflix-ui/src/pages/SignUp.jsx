d21f2bd1911e981fd9e0073149be3011b19f4512
Sheetal Umesh <sheetalgowda7777@gmail.com>
2024-01-17 12:15:20 +0000

    changes

--- netflix-ui/src/pages/SignUp.jsx
+++ netflix-ui/src/pages/SignUp.jsx
@@ -36,7 +36,7 @@ export default function SignUp() {
                 <div className="body flex column a-center j-center">
                     <div className="text flex column">
                         <h1> Unlimited movies , TV shows and more</h1>
-                        <h4> watch anywhere. Cancle anytime.</h4>
+                        <h4> Watch anywhere. Cancel anytime.</h4>
                         <h6>Ready to watch? Enter your email to create or restart membership</h6>
                     </div>
                     <div className="form">
